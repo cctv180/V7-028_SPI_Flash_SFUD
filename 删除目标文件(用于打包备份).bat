d896d57bb5f587587d0b416af9ad3f9f6365bd68
cctv180 <cctv180@gmail.com>
2020-04-06 01:07:43 +0800

    整理文件

--- 删除目标文件(用于打包备份).bat
+++ 删除目标文件(用于打包备份).bat
@@ -3,8 +3,14 @@ rd /Q /S project\MDK-ARM(uV5)\Listings
 rd /Q /S project\MDK-ARM(uV5)\DebugConfig
 del /Q project\MDK-ARM(uV5)\*.bak
 del /Q project\MDK-ARM(uV5\*.dep
-del /Q project\MDK-ARM(uV5)\JLink*
+del /Q Project\MDK-ARM(uV5)\*.uvguix.*
+del /Q Project\MDK-ARM(uV5)\*.scvd
+#del /Q project\MDK-ARM(uV5)\JLink*
+del /Q project\MDK-ARM(uV5)\JLinkLog.txt
 del /Q project\MDK-ARM(uV5)\project.uvgui.*
+
+rd  /Q /S Project\EmbeddedProject1\VisualGDB
+del /Q Project\EmbeddedProject1\*.vcxproj.user
 
 del /Q project\EWARMv8\Project.dep
 del /Q project\EWARMv8\Debug
